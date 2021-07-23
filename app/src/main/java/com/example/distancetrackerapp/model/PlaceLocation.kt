4df287936dee2de72af893d2f7f92e1edf06309e
guillermo <guillermodam3@gmail.com>
2021-07-23 12:01:39 +0200

    Places Autofill autocom plete fragments finished triger when there are 2 loccations makes polilyne etc...

---
+++ app/src/main/java/com/example/distancetrackerapp/model/PlaceLocation.kt
@@ -0,0 +1,8 @@
+package com.example.distancetrackerapp.model
+
+
+data class PlaceLocation(val id: String, val name: String, val latitude: Double, val longitude: Double) {
+
+
+
+}
---
+++ app/src/main/java/com/example/distancetrackerapp/util/NetworkListener.java
@@ -0,0 +1,5 @@
+package com.example.distancetrackerapp.util;
+
+public class NetworkListener {
+
+}
